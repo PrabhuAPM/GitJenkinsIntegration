70217f74442d8e03473f329614062f1fa0728a8f
ALLAMAPRABHU <ALLAMAPRABHU@LAPTOP-J620CCJO>
2017-09-15 15:46:51 +0530

    My First commit

---
+++ src/main/java/com/allam/automationgit/Testjunit.java
@@ -0,0 +1,18 @@
+package com.allam.automationgit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testjunit {
+
+	WebDriver driver=null;
+	@Test
+	public void sampletest()
+	{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Hi Github and Jenkins");
+	}
+}
